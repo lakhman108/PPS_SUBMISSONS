ee53d1eff12e66347dcf96c604a1b1fdf365c092
lakhman108 <119241790+lakhman108@users.noreply.github.com>
2023-04-13 07:35:12 +0530

    PPS_SUBMISSONS

--- PPSII_CE081_LAB5/5.cpp
+++ PPSII_CE081_LAB5/5.cpp
@@ -1,63 +1,54 @@
 #include<iostream>
+#include <string>
 
 using namespace std;
+int bonus_e=0,bonus_t=0,bonus_m=0;
 
-class bonus
+class person
 {
-    private:
-    int salary[100];
-    char c;
-    string name[100];
-    int bouns_e=0;
-    int bonus_t=0;
-    int bonus_m=0;
-    public:
-   int member=0;
-    void initialize(int salary,char c,string name)
-    {
-        this->salary[member]=salary;
-        this->c=c;
-        this->name[member]=name;
-        member++;
-        if (this->c=='E')
-        {
-            bouns_e+=salary*0.25;
-        }
-        else if (this->c=='T')
-        {
-            bonus_t+=salary*0.28;
-        }
-        else if (this->c=='M')
-        {
-            bonus_m+=salary*0.3;
-        }
-     
-    }
-void print(){
-    for(int i=0;i<member;i++){
-        cout<<name[i]<<" "<<salary[i]<<endl;
-    }
-    cout<<bouns_e<<" ";    
-    cout<<bonus_t<<" ";
-    cout<<bonus_m<<endl;
+char x;int salary;string name;
+public:
+void set(char x,int salary,string name)
+{
+    this->name=name;
+    this->salary=salary;
+    this->x=x;
+    if(this->x=='E')
+    bonus_e+=(salary*0.25);
+    if(this->x=='T')
+    bonus_t+=(salary*0.28);
+    if(this->x=='M')
+    bonus_m+=(salary*0.3);
+   // cout<<this->x<<endl;
 }
-    
-
+void print()
+{
+    cout<<name<<" "<<salary<<endl;
+}
+void bonus()
+{
+    cout<<bonus_e<<" "<<bonus_t<<" "<<bonus_m<<" "<<(bonus_e+bonus_m+bonus_t)<<endl;
+}
 };
 
-int main()
-{bonus A;
-int t;
-cin>>t;
-   for (int i = 0; i < t; i++)
-   {
-     char c;
-    int salary;
-    string name;
-    cin>>c>>salary>>name;
- A.initialize(salary,c,name);
-   }
-   A.print();
 
+
+
+int main()
+{int t;
+ cin>>t;
+   person p1[t];
+    for (int i = 0; i < t; i++)
+    {
+        char x;int salary;string name;
+    cin >> x;
+       
+        cin>>salary;
+       
+       getline(cin,name);
+     p1[i].set(x,salary,name);
+     p1[i].print();
+    }
+    p1[t-1].bonus();
     return 0;
 }
